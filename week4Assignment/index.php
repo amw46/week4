acd0a62ac33bc08a8d3d4710d990f42ba3b28699
Alice M Woodruff <amw46@afsconnect1.njit.edu>
2018-02-09 12:50:21 -0500

    if-elseif strcmp statement

--- week4Assignment/index.php
+++ week4Assignment/index.php
@@ -1,12 +1,21 @@
 <?php
 	$date =  date('Y-m-d', time());
 	$dateNew = str_replace("-", '/', $date); //'-' in $date replaced with '/'
-	echo "The value of \$date: ".$date."<br>";
+	echo "The value of \$date: ".$dateNew."<br>";
 	$tar = "2017/05/24";
 	echo "The value of \$tar: ".$tar."<br>";
 	$year = array("2012", "396", "300","2000", "1100", "1089");
 	echo "The value of \$year: ";
 	print_r($year);
+
+	if(strcmp($dateNew, $tar) < 0) { //if $dateNew is less than $tar, result is -1
+		echo "The past";
+	else if (strcmp($dateNew, $tar) > 0) { //if dateNew is greater than tar, result is 1
+		echo "The future";
+	}
+	else {
+		echo "Oops."
+	}
 
 	echo "test";
 ?>
